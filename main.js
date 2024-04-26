f207de78ffc64b497340a327ea7958d75fb1d1fa
Musawer Khan <musawerbangash109@gmail.com>
2024-04-26 20:57:54 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let weather = "cloudy";
+if (weather === "raining") {
+    console.log("wear a rain coat");
+}
+else if (weather === "cloudy") {
+    console.log("wear a light coat");
+}
+else {
+    console.log("wear a sunglasses");
+}
---
+++ main.ts
@@ -0,0 +1,14 @@
+import { log } from "console";
+
+let weather = "cloudy";
+if (weather === "raining") {
+    console.log("wear a rain coat"); 
+} else if (weather === "cloudy") {
+    console.log("wear a light coat");
+ }
+ else {
+    console.log("wear a sunglasses");
+    
+}
+
+
